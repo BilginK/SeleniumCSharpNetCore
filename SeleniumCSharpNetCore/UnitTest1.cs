5c52e8c8f5a5387f15ad1d13a62aa609d272db21
Bilgin KALMIS <bilgin.kalmis@alten.nl>
2022-08-24 16:24:42 +0200

    Initial Commit combo controls
    Initial Commit combo controls

---
+++ SeleniumCSharpNetCore/UnitTest1.cs
@@ -0,0 +1,30 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumCSharpNetCore
+{
+    public class Tests
+    {
+        public IWebDriver Driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("Setup");
+            ChromeOptions options = new ChromeOptions();
+            Driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+
+            Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
+            Driver.FindElement(By.XPath("(//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1'])[1]")).Click();
+            
+            Console.WriteLine("Test1");
+            Assert.Pass();
+        }
+    }
+}
