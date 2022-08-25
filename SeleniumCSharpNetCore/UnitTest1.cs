8943857d05e7dd260fab3470521897bea29d882a
Bilgin KALMIS <bilgin.kalmis@alten.nl>
2022-08-25 13:41:51 +0200

    changed to my Test

--- SeleniumCSharpNetCore/UnitTest1.cs
+++ SeleniumCSharpNetCore/UnitTest1.cs
@@ -1,38 +1,41 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using WebDriverManager.DriverConfigs.Impl;
 
 namespace SeleniumCSharpNetCore
 {
     public class Tests
     {
-        public IWebDriver Driver;
+        public IWebDriver driver;
 
         [SetUp]
         public void Setup()
         {
             Console.WriteLine("Setup");
             ChromeOptions options = new ChromeOptions();
-            Driver = new ChromeDriver();
-            Driver.Manage().Window.Maximize();
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+                      
         }
-
-
-
+        
         [Test]
         public void Test1()
         {
-            Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+            driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
 
-            Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
+            driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
             //Driver.FindElement(By.XPath("(//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1'])[1]")).Click();
 
             Console.WriteLine("Test 1");
             Assert.Pass();
         }
+
         [TearDown]
-        public void AfterClose()
+        public void AfterTest()
         {
-            Driver.Quit();
+            driver.Quit();
         }
     }
 }
